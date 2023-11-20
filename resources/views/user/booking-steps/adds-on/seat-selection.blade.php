e4b7cfb14343b8ba824b88fe5497174b0625b923
Jericho <jerichobantiquete@gmail.com>
2023-11-20 11:49:31 +0800

    guide to selection seat

--- resources/views/user/booking-steps/adds-on/seat-selection.blade.php
+++ resources/views/user/booking-steps/adds-on/seat-selection.blade.php
@@ -1,22 +1,51 @@
 <div id="seat_card-{{ $i }}" class="card py-4 mt-4 mx-auto rounded-1 container" style="display: none;">
-    <h2 class=" pb-4">Seat Selection</h2>
-    @foreach(array_chunk($seats, 5) as $index => $chunk)
+    <h2 class="pb-4">Seat Selection</h2>
+
     <div class="row">
-        @foreach($chunk as $seat)
-        <div class="col-md-2 mx-auto">
-            <label for="{{ $seat }}{{ $i }}"
-                class="card mb-2 rounded-2 {{ $index % 4 == 0 ? 'bg-yellow' : ($index % 4 == 1 ? 'bg-blue' : ($index % 4 == 2 || $index % 4 == 3 ? 'bg-gray' : 'bg-blue')) }}">
+        <!-- Legends (Left Side) -->
+        <div class="col-md-3 mx-auto">
+            <h5>Guide to selection seat</h5>
+            <div class="card mb-2 rounded-2 bg-yellow">
                 <div class="card-body">
-                    {{-- <input type="checkbox" id="{{ $seat }}"> --}}
-                    <input value="{{ $seat }}" name="seat[]" type="checkbox" id="{{ $seat }}{{ $i }}" {{ in_array($seat,
-                        $acquiredSeats) ? 'checked' : '' }} {{ in_array($seat, $acquiredSeats) ? 'disabled' : '' }}>
-                    <span>{{ $seat }}</span>
+                    <span>Premium Seat</span>
+                    <p>PHP 390.00</p>
                 </div>
-            </label>
+            </div>
+            <div class="card mb-2 rounded-2 bg-blue">
+                <div class="card-body">
+                    <span>Standard Plus Seat</span>
+                    <p>PHP 245.00</p>
+                </div>
+            </div>
+            <div class="card mb-2 rounded-2 bg-gray">
+                <div class="card-body">
+                    <span>Standard Seat</span>
+                    <p>PHP 200.00</p>
+                </div>
+            </div>
         </div>
-        @endforeach
+
+        <!-- Seat Selection (Right Side) -->
+        <div class="col-md-9">
+            @foreach(array_chunk($seats, 5) as $index => $chunk)
+            <div class="row">
+                @foreach($chunk as $seat)
+                <div class="col-md-2 mx-auto">
+                    <label for="{{ $seat }}{{ $i }}"
+                        class="card mb-2 rounded-2 {{ $index % 4 == 0 ? 'bg-yellow' : ($index % 4 == 1 ? 'bg-blue' : ($index % 4 == 2 || $index % 4 == 3 ? 'bg-gray' : 'bg-blue')) }}">
+                        <div class="card-body">
+                            <input value="{{ $seat }}" name="seat[]" type="checkbox" id="{{ $seat }}{{ $i }}" {{
+                                in_array($seat, $acquiredSeats) ? 'checked' : '' }} {{ in_array($seat, $acquiredSeats)
+                                ? 'disabled' : '' }}>
+                            <span>{{ $seat }}</span>
+                        </div>
+                    </label>
+                </div>
+                @endforeach
+            </div>
+            @endforeach
+        </div>
     </div>
-    @endforeach
 </div>
 
 <style>
